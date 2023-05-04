6478ad8ea34ad7a74b27af592a71cef7e7732671
muhamdan <danhamx2@gmail.com>
2023-05-04 18:05:06 +0700

    Create OperatorUnary.java

---
+++ ptm03/src/main/java/operator/OperatorUnary.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operator;
+
+/**
+ *
+ * @author user
+ */
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+        
+        
+    }
+ 
+}
