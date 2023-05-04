23cbc5b73ad03c09e8d1dd87561b88fb43dc36c6
muhamdan <danhamx2@gmail.com>
2023-05-04 17:35:58 +0700

    Create Ptm03.java

---
+++ ptm03/src/main/java/ptm03/Ptm03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ptm03;
+
+/**
+ *
+ * @author user
+ */
+public class Ptm03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
