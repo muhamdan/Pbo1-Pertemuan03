0ea924961a9e1b760bfc182797935e2e924f7693
muhamdan <danhamx2@gmail.com>
2023-05-04 18:26:58 +0700

    Create Switch.java

---
+++ ptm03/src/main/java/percabangan/Switch.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package percabangan;
+
+/**
+ *
+ * @author user
+ */
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            case 5:
+                System.out.println("mahluk halus");
+                break;
+            }
+    }
+}
