68adb5ba541d20b96dbeeedadd743427efec889a
muhamdan <danhamx2@gmail.com>
2023-05-04 17:36:43 +0700

    Create OperasiString.java

---
+++ ptm03/src/main/java/string/OperasiString.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package string;
+
+/**
+ *
+ * @author user
+ */
+public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Banjar";
+        
+        System.out.println("kota");
+    }
+}
