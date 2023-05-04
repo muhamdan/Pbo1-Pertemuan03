887f859e6d769abe583729ac0fbf7731e0c88c86
muhamdan <danhamx2@gmail.com>
2023-05-04 18:22:12 +0700

    Update IfElse.java

--- ptm03/src/main/java/percabangan/IfElse.java
+++ ptm03/src/main/java/percabangan/IfElse.java
@@ -15,8 +15,11 @@ public class IfElse {
         
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("uang cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang cukup, berelaan lah");
+            System.out.println("Uang pas.. UwU..");
          }
     }
 }
