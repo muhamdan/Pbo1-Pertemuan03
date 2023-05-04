fdba8e996479c665688020b2f969e6046831de07
muhamdan <danhamx2@gmail.com>
2023-05-04 18:01:24 +0700

    Create OperatorBinary.java

---
+++ ptm03/src/main/java/operator/OperatorBinary.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operator;
+
+/**
+ *
+ * @author user
+ */
+public class OperatorBinary {
+    String nama = "Muhammad Hamdani";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+    
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+        
+    }
+}
