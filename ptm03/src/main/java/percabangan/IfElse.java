b36a61a0e9b87df37f0f540e6fb9e98097fa3278
muhamdan <danhamx2@gmail.com>
2023-05-04 18:17:38 +0700

    Create IfElse.java

---
+++ ptm03/src/main/java/percabangan/IfElse.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package percabangan;
+
+/**
+ *
+ * @author user
+ */
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+        
+        if(uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kurangi jajan anda");
+        }else{
+            System.out.println("Uang cukup, selamat menikmati");
+         }
+    }
+}
