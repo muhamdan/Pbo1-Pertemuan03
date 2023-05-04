1632d0e40e3aec299f2515872faf20a051934575
muhamdan <danhamx2@gmail.com>
2023-05-04 17:52:22 +0700

    Update OperasiString.java

--- ptm03/src/main/java/string/OperasiString.java
+++ ptm03/src/main/java/string/OperasiString.java
@@ -12,6 +12,15 @@ public class OperasiString {
     public static void main(String[] args) {
         String kota = "Banjar";
         
-        System.out.println("kota");
+        System.out.println(kota);
+        
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+        
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
     }
 }
