d58a4ac2e335695b2e5a4966ceb8e27c216f1e39
muhamdan <danhamx2@gmail.com>
2023-05-04 18:11:19 +0700

    Create OperatorEquality.java

---
+++ ptm03/src/main/java/operator/OperatorEquality.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operator;
+
+/**
+ *
+ * @author user
+ */
+public class OperatorEquality {
+    public static void main(String[] args) {
+        int i = 5;
+        intt j = 6;
+        System.out.println(i==5);
+        System.out.println(i==6);
+        System.out.println(i==j);
+        System.out.println(i>j);
+        System.out.println(i<j);
+        System.out.println(5>6);
+        System.out.println(5<6);
+        System.out.println(i!=5);
+        System.out.println(j!=5);
+    }
+}
